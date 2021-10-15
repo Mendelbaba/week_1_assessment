bccde8d0e19df767a8393f153ff7e9fb16b9d533
Mendelbaba <mendybaba@gmail.com>
2021-10-15 15:41:55 -0400

    finished variables

--- sldc.js
+++ sldc.js
@@ -5,15 +5,22 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Analysis of Requirements
-// Design
-// Implementation
-// Maintain
-// Planning
-// Testing & Integration
+// 1 Analysis of Requirements
+// 2 ? Planning
+// 3 Design
+// 4 ? Implementation
+// 5 Testing & Integration
+// 6 Maintain
+//
+// 
 
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
-*/
+*/ let analysis = "analyse the the clients requirements necessary to put out the first functioning version of the application ";
+  let planning = "plan out the steps necessary to build the application according to the requirements in step 1 (not the coding aspect but the teams and communication and protocols";
+  let design = "design and agree upon a road map of how this software will be built in regards to the stucture and code of the software";
+  let implementation = " a specific block of time dedicated to actually buillding ou the code, i believe it was refferd to as a sprint";
+  let testingAndIntegration = "Test the software and show it to the client in order to show that the goals for this sprint were achieved, and to take notes on what can be improved upon for a possible next sprint";
+let maintain = "set forth the protocols for launching the application, and the maintenance and support necessary to kep it up and running";
